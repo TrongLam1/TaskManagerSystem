cea3ed0a3601ef89850daa9196b5560f504606ba
Trong Lam <tronglam199015@gmail.com>
2025-02-03 13:15:57 +0700

    Init project

---
+++ TaskManager-BE-SpringBoot/src/test/java/com/taskManagerSystem/TaskManagerSystem/TaskManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskManagerSystem.TaskManagerSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
